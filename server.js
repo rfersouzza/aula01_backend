17372d2da7dcfa765a31dc70b1cbe9a3dd03b6e0
Renata Souza <renatafersouzza@gmail.com>
2026-05-21 15:28:36 -0300

    segunda aula de backend

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Pagina inicial com Express');
+});
+
+app.get('/sobre', (req, res) => {
+    res.send('Pagina sobre com Express');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Pagina de contato com Express');
+});
+
+app.get('/produtos', (req, res) => {
+    res.send('Lista de produtos');
+});
+
+app.get('/servicos', (req, res) => {
+    res.send('Lista de serviços');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
